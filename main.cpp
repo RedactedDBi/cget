867e87eb46bfc6e4f49172950f6fc60a84e423ff
Eric Lavesson <eric.lavesson@gmail.com>
2016-07-31 23:17:50 +0200

    Print a simple usage/info prompt

--- main.cpp
+++ main.cpp
@@ -1,3 +1,29 @@
+#include <string>
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+namespace {
+    const std::string USAGE =
+            "\n"
+            "Usage: cget <command> [flags] [args...]\n"
+            "\n"
+            "The most commonly used cget commands are:\n"
+            "   install      Install a dependency\n"
+            "   uninstall    Uninstall a dependency\n"
+            "   update       Recursively check dependencies for newer versions\n"
+            "\n"
+            "Example usages:\n"
+            "\n"
+            "cget install example/dep         # Install latest registered semver\n"
+            "cget install example/dep:^5.0    # Install v5.0 or greater\n"
+            "cget install example/dep:~5.0    # Install latest version within v5.0\n"
+            "\n";
+}
+
 int main(int argc, char* argv[]) {
+    cout << USAGE;
     return 0;
 }
